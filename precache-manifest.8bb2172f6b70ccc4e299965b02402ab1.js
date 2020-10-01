4eba6cf6a116bb603f426336e6358fea332fb566
Danilo Aleksandric <aleksandric.danilo@gmail.com>
2020-10-01 02:03:31 -0400

    Updates

---
+++ precache-manifest.8bb2172f6b70ccc4e299965b02402ab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bebb6834c923591fd8eb68c42ce50f",
+    "url": "/danilo-aleksandric-project-five/index.html"
+  },
+  {
+    "revision": "481985bbcf99cf70910a",
+    "url": "/danilo-aleksandric-project-five/static/css/main.1251fccb.chunk.css"
+  },
+  {
+    "revision": "4dbc3331ed9f3165e1d0",
+    "url": "/danilo-aleksandric-project-five/static/js/2.ce4e64dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/danilo-aleksandric-project-five/static/js/2.ce4e64dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481985bbcf99cf70910a",
+    "url": "/danilo-aleksandric-project-five/static/js/main.129eaba4.chunk.js"
+  },
+  {
+    "revision": "d3d1fc42e94e8239a058",
+    "url": "/danilo-aleksandric-project-five/static/js/runtime-main.38053c98.js"
+  },
+  {
+    "revision": "eeedab03215deffee24e4071dac980b1",
+    "url": "/danilo-aleksandric-project-five/static/media/7747720.eeedab03.jpg"
+  }
+]);
